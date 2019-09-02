1984081883b239378ebf1b8ef9978a14deb0c789
Naynesh Rathod <nayneshrathod@gmail.com>
2019-09-02 15:02:43 +0530

    new angular

---
+++ my-new-example/src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  clickCounter = 0;
+  name = '';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  countClick() {
+    this.clickCounter += 1;
+  }
+
+  setClassess() {
+    let myClasses = {
+      active: this.clickCounter > 4,
+      notactive: this.clickCounter <= 4,
+    }
+    return myClasses;
+  }
+}
---
+++ my-new-example/src/app/list/list.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+  obj1: object;
+  obj = [
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 20, name: 'Khumesh Raghunath Lohar', city: 'Shirpur', country: 'india', phone: 7219767226, website_url: 'http://www.google.com'},
+    {id: 26, name: 'naynesh raghunath rathod', city: 'pashte', country: 'india', phone: 8550949497, website_url: 'http://www.google.com'},
+    {id: 74, name: 'Prakash Amrutbhai Koli', city: 'NImbhel', country: 'india', phone: 8475241536, website_url: 'http://www.google.com'},
+    {id: 5, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214},
+    {id: 3, name: 'Sentence 1', city: 'zxy', country: 'india', phone: 85452145214}
+];
+
+
+  constructor(private Http: HttpService) {
+  }
+
+  ngOnInit() {
+    this.Http.myMethod();
+    this.Http.getdata().subscribe(data => {
+      this.obj1 = data;
+      console.log(this.obj);
+    });
+  }
+}
